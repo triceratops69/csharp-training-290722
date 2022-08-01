8c101ded50d5ff1e1d53ce399fa26e9b09efaff5
trice <trice@DESKTOP-N6JN2TJ>
2022-08-01 14:33:28 +0300

    Задание №2: Создать первый работающий тест. Попытка 2.

--- addressbook-web-tests/GroupCreationTests.cs
+++ addressbook-web-tests/GroupCreationTests.cs
@@ -7,10 +7,10 @@ using OpenQA.Selenium;
 using OpenQA.Selenium.Firefox;
 using OpenQA.Selenium.Support.UI;
 
-namespace addressbook_web_tests
+namespace WebAddressbookTests
 {
     [TestFixture]
-    public class UnitTest2
+    public class GroupCreationTests
     {
         private IWebDriver driver;
         private StringBuilder verificationErrors;
@@ -40,7 +40,7 @@ namespace addressbook_web_tests
         }
 
         [Test]
-        public void NewGroupCreate()
+        public void GroupCreationTest()
         {
             driver.Navigate().GoToUrl(baseURL);
             driver.FindElement(By.Name("user")).Click();
